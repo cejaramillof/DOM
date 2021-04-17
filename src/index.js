f9c90ee79fc89a5d12afc5b5fdf1798e9287235e
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2021-04-17 18:50:46 -0500

    add info attributes to tags, and add app container

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@
 
 console.log('Happy hacking :)');
 
-const url = 'https://platzi-avo.vercel.app/api/avo';
+const baseUrl = 'https://platzi-avo.vercel.app';
+
+const appNode = document.querySelector('#app')
 
 // Web Api 1. Connect to server, 2. Process response and parse to JSON, 3. JSON -> DATA -> Render
 window
-  .fetch(url)
+  .fetch(`${baseUrl}/api/avo`)
   .then((response) => response.json())
   .then(({data}) => {
     const items = [];
     data.forEach(item => {
       const img = document.createElement('img');
+      img.src = `${baseUrl}${item.image}`;
       // document.body.appendChild(imagen);
       const title = document.createElement('h2');
+      title.textContent = item.name;
       const price = document.createElement('span');
+      price.textContent = item.price;
 
       const container = document.createElement('div');
       container.append(img, title, price) // appendChild each item
@@ -25,5 +30,6 @@ window
       // document.body.appendChild(container);
       items.push(container);
     })
-    document.body.append(...items);
+    // document.body.append(...items);
+    appNode.append(...items);
   });
